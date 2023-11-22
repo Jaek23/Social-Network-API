8e783ad11d806b64f50734d35f37b6b7cdc5ed89
jae kim <jaehkim23@gmail.com>
2023-11-21 21:04:54 -0600

    added model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const {Schema, model} = require('mongoose');
+
+//User Schema 
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+
+        email:{
+            type: String,
+            required: true,
+            unique:true,
+            validate:{
+                validator:function(v){
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                }
+            }
+        },
+
+        thoughts:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+
+        friends:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:'Thought'
+            }
+        ],
+    },
+    {
+        toJSON:{
+            virtuals:true,
+        },
+        id:false,
+    }
+);
+
+
+//Virtual property 'friendCount'
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User; 
